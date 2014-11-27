0fc034cddd015291f1aad16277b04b1d7520da47
Designercode <april.cummings1@gmail.com>
2014-11-27 11:36:38 +0000

    exploring jQuery functionality

--- app/scripts/landing.js
+++ app/scripts/landing.js
@@ -3,7 +3,34 @@ $(document).ready(function() {
       var subText = $(this).text();
        $(this).text(subText + "!");
     });
+
+
+  var fadeOut = function(event){
+    console.log("You clicked on the Bloc Jams Header.");
+    $(this).fadeOut("slow");
+  }
+  
+  $('nav').click(fadeOut);
+
+  var changeColorOn = function(event) {
+    $(this).css("color","green");
+  };
+
+  var changeColorOff = function(event) {
+    $(this).css("color","white")
+  };
+
+    $('.hero-content h3').hover(changeColorOn, changeColorOff);
  
+   
+  var changeFontSize = function(event) {
+    console.log("Click-action triggered.");
+    $(this).css({'font-size': '30px'});
+  };
+
+    $('h5').click(changeFontSize);
+
+
    var onHoverAction = function(event) {
      console.log('Hover action triggered.');
      $(this).animate({'margin-top': '10px'});
@@ -16,3 +43,4 @@ $(document).ready(function() {
  
     $('.selling-points .point').hover(onHoverAction, offHoverAction);
   });
+
